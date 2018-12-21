be421755dcd3703cb3fabbe90386e82e57470f7e
SimonMcDonnell <smcdonnell2195@gmail.com>
2018-12-21 12:00:24 +0000

    Finished linear model reworking

--- Abalone/eval_models.py
+++ Abalone/eval_models.py
@@ -7,6 +7,9 @@ import sys
 sys.path.insert(0, '../seal_wrapper/')
 from seal_wrapper import EA
 
+X_train, X_val, X_test, y_train, y_val, y_test = prepare_dataset()
+X_test_enc = EA(X_test.values, True)
+
 # mean sum squared error
 def msse(pred, y):
     return np.sum((y - pred)**2) / y.shape[0]
@@ -20,27 +23,34 @@ def normalize_bias(b, mean, var, shift, scale):
     return (b - mean) * (scale / np.sqrt(var + 0.001)) + shift
 
 
-def plot_predictions(pred, y, title):
+def plot_predictions(pred_clear, pred_enc, y, title):
     fig = plt.figure(figsize=(6, 6))
-    ax = plt.subplot(111)
-    ax.scatter(pred, y, alpha=0.2)
-    ax.title(title)
-    ax.set_xlabel('predicted')
-    ax.set_ylabel('true')
-    ax.plot(np.arange(0, 30, 0.1), np.arange(0, 30, 0.1))
+    # clear
+    ax1 = plt.subplot(121)
+    ax1.scatter(pred_clear, y, alpha=0.2)
+    ax1.set_title(title + ' - clear')
+    ax1.set_xlabel('predicted')
+    ax1.set_ylabel('true')
+    ax1.plot(np.arange(0, 30, 0.1), np.arange(0, 30, 0.1))
+    # encrypted
+    ax2 = plt.subplot(122)
+    ax2.scatter(pred_enc, y, alpha=0.2)
+    ax2.set_title(title + ' - enc')
+    ax2.set_xlabel('predicted')
+    ax2.set_ylabel('true')
+    ax2.plot(np.arange(0, 30, 0.1), np.arange(0, 30, 0.1))
+    plt.show()
 
 
 def eval_linear():
-    X_train, X_val, X_test, y_train, y_val, y_test = prepare_dataset()
     weights = np.load('linear_weights.npy')
     w1, b1, scale, shift, mean, std, w2, b2 = weights
     w1 = normalize_weights(w1, std**2, scale)
     b1 = normalize_bias(b1, mean, std**2, shift, scale).reshape(1, -1)
     # clear
     l1_clear = X_test.values.dot(w1) + b1
-    pred_clear = np.dot(l1, w2) + b2
+    pred_clear = np.dot(l1_clear, w2) + b2
     # encrypted
-    X_test_enc = EA(X_test.values, True)
     w1_enc = EA(w1)
     b1_enc = EA(b1.reshape(1, -1))
     w2_enc = EA(w2)
@@ -50,5 +60,4 @@ def eval_linear():
     # report predictions
     print('MSSE clear: {}'.format(msse(pred_clear.flatten(), y_test)))
     print('MSSE enc: {}'.format(msse(pred_enc.flatten(), y_test)))
-    plot_predictions(pred_clear.flatten(), y_test, 'Linear - clear')
-    plot_predictions(pred_enc.flatten(), y_test, 'Linear - enc')
+    plot_predictions(pred_clear.flatten(), pred_enc.flatten(), y_test, 'Linear')
