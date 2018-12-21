e99528bde2490839bc0a87251675670fe5a1e543
SimonMcDonnell <smcdonnell2195@gmail.com>
2018-12-21 11:48:24 +0000

    Updated files

--- Abalone/eval_models.py
+++ Abalone/eval_models.py
@@ -12,6 +12,14 @@ def msse(pred, y):
     return np.sum((y - pred)**2) / y.shape[0]
 
 
+def normalize_weights(w, var, scale):
+    return w * scale / np.sqrt(var + 0.001)
+
+
+def normalize_bias(b, mean, var, shift, scale):
+    return (b - mean) * (scale / np.sqrt(var + 0.001)) + shift
+
+
 def plot_predictions(pred, y, title):
     fig = plt.figure(figsize=(6, 6))
     ax = plt.subplot(111)
@@ -25,15 +33,20 @@ def plot_predictions(pred, y, title):
 def eval_linear():
     X_train, X_val, X_test, y_train, y_val, y_test = prepare_dataset()
     weights = np.load('linear_weights.npy')
-    print(weights)
-    return
+    w1, b1, scale, shift, mean, std, w2, b2 = weights
+    w1 = normalize_weights(w1, std**2, scale)
+    b1 = normalize_bias(b1, mean, std**2, shift, scale).reshape(1, -1)
     # clear
-    pred_clear = np.dot(X_test, w) + b
+    l1_clear = X_test.values.dot(w1) + b1
+    pred_clear = np.dot(l1, w2) + b2
     # encrypted
     X_test_enc = EA(X_test.values, True)
-    w_enc = EA(w)
-    b_enc = EA(b.reshape(1, -1))
-    pred_enc = (X_test_enc.dot(w_enc) + b_enc).values()
+    w1_enc = EA(w1)
+    b1_enc = EA(b1.reshape(1, -1))
+    w2_enc = EA(w2)
+    b2_enc = EA(b2.reshape(1, -1))
+    l1_enc = X_test_enc.dot(w1_enc) + b1_enc
+    pred_enc = (l1_enc.dot(w2_enc) + b2_enc).values()
     # report predictions
     print('MSSE clear: {}'.format(msse(pred_clear.flatten(), y_test)))
     print('MSSE enc: {}'.format(msse(pred_enc.flatten(), y_test)))
