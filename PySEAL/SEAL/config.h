b9ad66370cb168441439eb4e9f4909709b149b0f
SimonMcDonnell <smcdonnell2195@gmail.com>
2019-01-18 15:29:59 +0000

    New files

---
+++ PySEAL/SEAL/config.h
@@ -0,0 +1,6 @@
+/* config.h.  Generated from config.h.in by configure.  */
+#undef SEAL_ENABLE_INTRIN
+#undef SEAL_ENABLE___BUILTIN_CLZLL
+#undef SEAL_ENABLE___INT128
+#undef SEAL_ENABLE__ADDCARRY_U64
+#undef SEAL_ENABLE__SUBBORROW_U64
---
+++ PySEAL/SEAL/obj/seal/base64.d
@@ -0,0 +1,1 @@
+obj/base64.o obj/seal/base64.d : seal/base64.cpp seal/base64.h
